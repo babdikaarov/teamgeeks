90abc631150f0bff864d1d9ff56cb5275684d99f
Macbook Beks <babdikaarov@gmail.com>
2023-12-18 12:18:59 +0300

    initial file structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,11 @@
-// import "./app.scss";
 import { Outlet } from "react-router-dom";
 
 function App() {
    return (
       <>
-         <div>App</div>
          <Outlet />
       </>
    );
 }
 
 export default App;
-
-// <Route path="" element={<CoolBand />}>
-//     <Route index element={<Main />}></Route>
-//     <Route path="events" element={<Gallery />}>
-//         <Route index element={<GalleryCards />}></Route>
-//         {/* <Route index element={<GalleryCards2 />}></Route> */}
-//         <Route path=":id" element={<GalleryCard />}></Route>
-//     </Route>
-// </Route>
-// <Route path="studio" element={<CoolStudio />}></Route>
--- src/pages/coolBandShow/index.tsx
+++ src/pages/coolBandShow/index.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./header/components/Header";
 
 function CoolBandShow() {
    return (
       <>
-         <Header />
          <Outlet />
       </>
    );
