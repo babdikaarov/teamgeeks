0f4a3f76de338055821ae3e43637be0c1e02a6e4
Beksultan Abdikaarov <babdikaarov@gmail.com>
2023-12-21 03:44:00 +0000

    Revert "Merge branch 'COOL-21' into 'dev'"
    This reverts merge request !2

--- src/pages/coolBandShow/header/HeaderContainer.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../../shared/widgets/header/Header";
-import BandNavList from "../../../shared/UI/pageNavigation/BandNavList";
-import { lyrics, pageLogos, tempLinks } from "../../../temp/headerData";
-
-const HeaderContainer = () => {
-   return <Header pageLogos={pageLogos} lyrics={lyrics} links={tempLinks} NavList={BandNavList} />;
-};
-
-export default HeaderContainer;
--- src/pages/coolStudio/header/HeaderStudioContainer.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../../shared/widgets/header/Header";
-import StudioNavList from "../../../shared/UI/pageNavigation/StudioNavList";
-import { lyrics, pageLogos, tempLinks } from "../../../temp/headerData";
-
-const HeaderStudioContainer = () => {
-   return <Header pageLogos={pageLogos} lyrics={lyrics} links={tempLinks} NavList={StudioNavList} />;
-};
-
-export default HeaderStudioContainer;
