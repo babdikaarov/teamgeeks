a3b1d1e3ced0feae929b97f0d71e5a54681e1a1f
arlen isaev <arlenisaev43@gmail.com>
2023-12-22 21:26:08 +0600

    cool show band

--- src/pages/CoolBand/CoolBand.tsx
+++ src/pages/CoolBand/CoolBand.tsx
@@ -1,9 +1,11 @@
 import Gis2 from "../../components/BandComponents/Gis2/Gis2.tsx";
+import CoolShowBand from '../../components/BandComponents/CoolShowBand/CoolShowBand.tsx';
 
 const CoolBand = () => {
    return (
       <>
          <div>
+            <CoolShowBand/>
             <Gis2 />
          </div>
       </>
