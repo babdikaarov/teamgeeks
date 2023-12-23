cad235086dfc792fcb8a5f42f4fa0455a2877962
rina1309 <113968192+rina1309@users.noreply.github.com>
2023-12-23 20:51:19 +0600

    COOL-28 corrected

--- src/components/BandComponents/WhyUs/WhyUs.tsx
+++ src/components/BandComponents/WhyUs/WhyUs.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import icon from "../../../assets/Tick.png";
+import styles from "../../../scss/partials/coolBand/_whyus.module.scss";
 
 const InfoBlock: React.FC<{ title: string; text: string }> = ({ title, text }) => (
-   <div className="info">
-      <div className="infoBlock">
-         <div className="textH3">
+   <div className={styles.InfoBlockWrapper}>
+      <div className={styles.InfoBlock}>
+         <div className={styles.textH3}>
             <img className="icon" src={icon} alt="Icon" />
             {title}
          </div>
@@ -15,33 +16,28 @@ const InfoBlock: React.FC<{ title: string; text: string }> = ({ title, text }) =
 
 const WhyUs: React.FC = () => {
    return (
-      <div className="wrapper">
-         <h2 className="WhyUs">Почему выбирают нас?</h2>
+      <div className={styles.container}>
+         <div className={styles.WhyUs}>Почему выбирают нас?</div>
          <div className="container flexDiv">
-            <div className="info1">
-               <InfoBlock
-                  title="Профессионалы своего дела"
-                  text="Большой опыт всех участников коллектива, ответственность и креатив"
-               />
-            </div>
-            <div className="info2">
-               <InfoBlock
-                  title="Индивидуальный подход"
-                  text="Поможем реализовать любое пожелание к исполняемым композициям и жанру"
-               />
-            </div>
-            <div className="info3">
-               <InfoBlock
-                  title="Фирменный стиль и айдентика"
-                  text="Оригинальная подача и особая энергетика вкупе со сценическим образом, соответствующим вашему событию"
-               />
-            </div>
-            <div className="info4">
-               <InfoBlock
-                  title="Техническое сопровождение"
-                  text="Профессиональная настройка каждого инструмента, зажигательные ритмы, красочный и объемный звук"
-               />
-            </div>
+            <InfoBlock
+               title="Профессионалы своего дела"
+               text="Большой опыт всех участников коллектива, ответственность и креатив"
+            />
+
+            <InfoBlock
+               title="Индивидуальный подход"
+               text="Поможем реализовать любое пожелание к исполняемым композициям и жанру"
+            />
+
+            <InfoBlock
+               title="Фирменный стиль и айдентика"
+               text="Оригинальная подача и особая энергетика вкупе со сценическим образом, соответствующим вашему событию"
+            />
+
+            <InfoBlock
+               title="Техническое сопровождение"
+               text="Профессиональная настройка каждого инструмента, зажигательные ритмы, красочный и объемный звук"
+            />
          </div>
       </div>
    );
