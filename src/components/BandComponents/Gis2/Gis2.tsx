8271173f533e8c1f63aa82f072c8865bbdff4055
Shayirbek Abdymomunov <legionera803@gmail.com>
2023-12-23 20:41:16 +0600

    module classes are redone

--- src/components/BandComponents/Gis2/Gis2.tsx
+++ src/components/BandComponents/Gis2/Gis2.tsx
@@ -1,10 +1,10 @@
 import { load } from "@2gis/mapgl";
 import React, { useEffect } from "react";
-
+import styles from "../../../scss/partials/coolBand/_gis2.module.scss";
 const Gis2 = () => {
    const MapWrapper = React.memo(
       () => {
-         return <div id="map-container" className="mapDiv" style={{ width: "612px", height: "461px" }}></div>;
+         return <div id="map-container" className={styles.mapDiv} style={{ width: "612px", height: "461px" }}></div>;
       },
       () => true,
    );
@@ -30,18 +30,18 @@ const Gis2 = () => {
 
    return (
       <div className="container">
-         <div className="mainMapDiv">
-            <div className="innerMapDiv">
-               <h4 className="callToUs">Связаться с нами</h4>
-               <p className="music">
+         <div className={styles.mainMapDiv}>
+            <div className={styles.innerMapDiv}>
+               <h4 className={styles.callToUs}>Связаться с нами</h4>
+               <p className={styles.music}>
                   Не теряй связь с музыкой!
                </p>
-               <span className="toBook">
+               <span className={styles.toBook}>
                Чтобы забронировать выступление Coolshowband отправьте
                нам  сообщение на WhatsApp. Мы свяжемся с вами в ближайшее время
                и ответим на все интересующие вас вопросы
                </span>
-               <button className="btnCall">
+               <button className={styles.btnCall}>
                   Связаться с нами
                </button>
             </div>
--- src/components/BandComponents/WhyUs/WhyUs.tsx
+++ src/components/BandComponents/WhyUs/WhyUs.tsx
@@ -1,42 +1,43 @@
 import React from "react";
 import icon from "../../../assets/Tick.png";
+import styles from "../../../scss/partials/coolBand/_whyus.module.scss";
 
 const InfoBlock: React.FC<{ title: string; text: string }> = ({ title, text }) => (
-   <div className="info">
-      <div className="infoBlock">
-         <div className="textH3">
-            <img className="icon" src={icon} alt="Icon" />
+   <div className={styles.info}>
+      <div className={styles.infoBlock}>
+         <div className={styles.textH3}>
+            <img className={styles.icon} src={icon} alt="Icon" />
             {title}
          </div>
       </div>
-      <div className="text">{text}</div>
+      <div className={styles.text}>{text}</div>
    </div>
 );
 
 const WhyUs: React.FC = () => {
    return (
-      <div className="wrapper">
-         <h2 className="WhyUs">Почему выбирают нас?</h2>
-         <div className="container flexDiv">
-            <div className="info1">
+      <div className={styles.wrapper}>
+         <h2 className={styles.WhyUs}>Почему выбирают нас?</h2>
+         <div className={`container ${styles.flexDiv}`}>
+            <div className={styles.info1}>
                <InfoBlock
                   title="Профессионалы своего дела"
                   text="Большой опыт всех участников коллектива, ответственность и креатив"
                />
             </div>
-            <div className="info2">
+            <div className={styles.info2}>
                <InfoBlock
                   title="Индивидуальный подход"
                   text="Поможем реализовать любое пожелание к исполняемым композициям и жанру"
                />
             </div>
-            <div className="info3">
+            <div className={styles.info3}>
                <InfoBlock
                   title="Фирменный стиль и айдентика"
                   text="Оригинальная подача и особая энергетика вкупе со сценическим образом, соответствующим вашему событию"
                />
             </div>
-            <div className="info4">
+            <div className={styles.info4}>
                <InfoBlock
                   title="Техническое сопровождение"
                   text="Профессиональная настройка каждого инструмента, зажигательные ритмы, красочный и объемный звук"
