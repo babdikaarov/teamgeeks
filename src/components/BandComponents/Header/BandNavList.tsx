6a8ad3a8e7305ee225d20126711d75be20484791
Beksultan A <babdikaarov@gmail.com>
2023-12-26 04:17:24 +0300

    isActive issue fix

--- src/components/BandComponents/Header/BandNavList.tsx
+++ src/components/BandComponents/Header/BandNavList.tsx
@@ -1,37 +1,43 @@
 import { FC } from "react";
-import { HashLink, NavHashLink } from "react-router-hash-link";
+import { HashLink } from "react-router-hash-link";
 import styles from "../../../scss/partials/coolBand/_bandNavList.module.scss";
+import { useLocation } from "react-router-dom";
 
 interface BandNavListProps {
-   lyrics: string;
+  lyrics: string;
 }
 
 const BandNavList: FC<BandNavListProps> = ({ lyrics }) => {
-   return (
-      <ul className={styles.navigation}>
-         <li>
-            <NavHashLink
-               to={"/#home"}
-               className={({ isActive }) => (isActive ? styles.active : "")}
-               onClick={() => window.scrollTo(0, 0)}
-            >
-               Главная
-            </NavHashLink>
-         </li>
-         <li>
-            <HashLink to={"/#about"}>О нас</HashLink>
-         </li>
-         <li>
-            <NavHashLink to={"/gallery"}>Галерея</NavHashLink>
-         </li>
-         <li>
-            <HashLink to={"/#collaboration"}>Коллаборация</HashLink>
-         </li>
-         <li>
-            <a href={lyrics}>Репертуар</a>
-         </li>
-      </ul>
-   );
+  const { pathname } = useLocation();
+  console.log(pathname);
+
+  return (
+    <ul className={styles.navigation}>
+      <li>
+        <HashLink to={"/#home"} onClick={() => window.scrollTo(0, 0)} className={pathname === "/" ? styles.active : ""}>
+          Главная
+        </HashLink>
+      </li>
+      <li>
+        <HashLink to={"/#about"}>О нас</HashLink>
+      </li>
+      <li>
+        <HashLink
+          to={"/gallery"}
+          onClick={() => window.scrollTo(0, 0)}
+          className={pathname === "/gallery" ? styles.active : ""}
+        >
+          Галерея
+        </HashLink>
+      </li>
+      <li>
+        <HashLink to={"/#collaboration"}>Коллаборация</HashLink>
+      </li>
+      <li>
+        <a href={lyrics}>Репертуар</a>
+      </li>
+    </ul>
+  );
 };
 
 export default BandNavList;
