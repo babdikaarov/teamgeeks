47a3abd94b9de5d18994bf9e6d8abe2074e934a6
rina1309 <113968192+rina1309@users.noreply.github.com>
2023-12-28 22:36:19 +0600

    COOL-39

--- src/pages/CoolStudio/CoolStudio.tsx
+++ src/pages/CoolStudio/CoolStudio.tsx
@@ -1,8 +1,10 @@
+import AboutStudio from "../../components/StudioComponents/AboutStudio/AboutStudio";
 import HeroStudio from "../../components/StudioComponents/HeroStudio/HeroStudio";
 const CoolStudio = () => {
    return (
       <div>
          <HeroStudio />
+         <AboutStudio />
       </div>
    );
 };
