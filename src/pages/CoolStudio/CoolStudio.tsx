309e2dea126ed919962de6732e82999466318ef0
Ganyev Aman <ganyevaman7@gmail.com>
2023-12-28 23:21:54 +0600

    Students Feedback

--- src/pages/CoolStudio/CoolStudio.tsx
+++ src/pages/CoolStudio/CoolStudio.tsx
@@ -1,9 +1,11 @@
+import AboutStudio from "../../components/StudioComponents/AboutStudio/AboutStudio";
 import HeroStudio from "../../components/StudioComponents/HeroStudio/HeroStudio";
 import StudentsFeedback from "../../components/StudioComponents/StudentsFeedback/StudentsFeedback";
 const CoolStudio = () => {
    return (
       <div>
          <HeroStudio />
+         <AboutStudio/>
          <StudentsFeedback/>
       </div>
    );
