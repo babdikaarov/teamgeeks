32dea23082081734360e2c3f24d922b82b98ffb5
Beksultan A <babdikaarov@gmail.com>
2024-01-11 05:17:53 +0300

    COOL-105

--- src/UI/Gallery/GalleryCollage.tsx
+++ src/UI/Gallery/GalleryCollage.tsx
@@ -1,42 +1,44 @@
 import { FC, useState } from "react";
 import styles from "./gallery/_galleryCollage.module.scss";
 import Modal from "./modal/Modal";
-
-type Item = {
-   alt: string;
-   src: string;
-   view: string;
-};
-interface GalleryCollageProps {
-   items: (Item | undefined)[];
-}
+import { GalleryCollageProps } from "./types";
 
 const GalleryCollage: FC<GalleryCollageProps> = ({ items }) => {
-   const [indexImage, setIndexImage] = useState<number>(0);
+  const itemsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState(1);
+  // const startItemIndex = (currentPage - 1) * itemsPerPage;
+  const startItemIndex = 0;
+  const endItemIndex = currentPage * itemsPerPage;
+  const perPage = items.slice(startItemIndex, endItemIndex);
+  const [indexImage, setIndexImage] = useState<number>(0);
 
-   const handleOpen = (index: number) => {
-      const modal = document.getElementById("modal") as HTMLDialogElement;
-      //  console.log(index);
-      setIndexImage(index);
-      modal && modal.showModal();
-   };
+  const handleOpen = (index: number) => {
+    const modal = document.getElementById("modal") as HTMLDialogElement;
+    //  console.log(index);
+    setIndexImage(index);
+    modal && modal.showModal();
+  };
 
-   return (
+  const handleClick = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <>
       <div className={styles.collage}>
-         {items &&
-            items.map((data, i) => (
-               <div
-                  key={i}
-                  className={data && styles.collageItem + " " + styles[data.view]}
-                  onClick={() => handleOpen(i)}
-               >
-                  <p>{i}</p>
-                  <img src={data?.src} alt={data?.alt} />
-               </div>
-            ))}
-         <Modal images={items} i={indexImage} setIndexImage={setIndexImage} />
+        {perPage &&
+          perPage.map((data, i) => (
+            <div key={i} className={data && styles.collageItem + " " + styles[data.view]} onClick={() => handleOpen(i)}>
+              <img src={data?.src} alt={data?.alt} />
+            </div>
+          ))}
+        <Modal images={items} i={indexImage} setIndexImage={setIndexImage} />
       </div>
-   );
+      <button className={styles.paginationButton} onClick={handleClick}>
+        Далее
+      </button>
+    </>
+  );
 };
 
 export default GalleryCollage;
--- src/components/GalleryComponents/Collage/Collage.tsx
+++ src/components/GalleryComponents/Collage/Collage.tsx
@@ -3,12 +3,7 @@ import GalleryCollage from "../../../UI/Gallery/GalleryCollage";
 import getGalleryData from "../../../tempData/getGalleyData";
 import returnIcon from "../../../assets/icons/return";
 import styles from "./_collage.module.scss";
-
-interface GalleryItem {
-  alt: string;
-  src: string;
-  view: string;
-}
+import collageCreatePattern from "../../../modules/collageCreatePattern";
 
 // interface CollageProps {
 //   eventID: number;
@@ -27,62 +22,11 @@ const Collage = () => {
   window.scroll(0, 0);
   const navigate = useNavigate();
   const { id } = useParams();
+  // FIXME add when backend ready
   // const sorted = getGalleryData.find((el) => el.eventID === Number(id))!;
   const sorted = getGalleryData[Number(id)];
 
-  // function sortArrayWithPortrait(arr: GalleryItem[]): (GalleryItem | undefined)[] {
-  //   const portraitObjects = arr.filter((obj: { view: string }) => obj.view === "portrait");
-  //   const landscapeObjects = arr.filter((obj: { view: string }) => obj.view === "landscape");
-
-  //   const sortedArray = [];
-  //   // Alternate between portrait and landscape
-  //   while (portraitObjects.length || landscapeObjects.length) {
-  //     if (portraitObjects.length) {
-  //       sortedArray.push(portraitObjects.shift());
-  //     }
-
-  //     if (landscapeObjects.length) {
-  //       sortedArray.push(landscapeObjects.shift());
-  //       landscapeObjects.length && sortedArray.push(landscapeObjects.shift());
-  //     }
-  //   }
-  //   console.log(sortedArray);
-  //   return arr && sortedArray;
-  // }
-  function sortArrayWithPortrait(arr: GalleryItem[]): (GalleryItem | undefined)[] {
-    const portraitObjects = arr.filter((obj: { view: string }) => obj.view === "portrait");
-    const landscapeObjects = arr.filter((obj: { view: string }) => obj.view === "landscape");
-
-    const sortedArray = [];
-
-    const sequence = [
-      "portrait",
-      "landscape",
-      "landscape",
-      "landscape",
-      "portrait",
-      "portrait",
-      "portrait",
-      "landscape",
-    ];
-    let currentIndex = 0;
-
-    while (portraitObjects.length || landscapeObjects.length) {
-      if (sequence[currentIndex] === "portrait" && portraitObjects.length) {
-        sortedArray.push(portraitObjects.shift());
-      } else if (sequence[currentIndex] === "landscape" && landscapeObjects.length) {
-        sortedArray.push(landscapeObjects.shift());
-      }
-
-      currentIndex = (currentIndex + 1) % sequence.length;
-    }
-
-    // console.log(sortedArray);
-    return arr && sortedArray;
-  }
-
-  const sortedArray = sortArrayWithPortrait(sorted.items);
-  //   console.log(sortedArray);
+  const sortedArray = collageCreatePattern(sorted.items);
   return (
     <section className={styles.section}>
       <div className={styles.collageInfo}>
@@ -90,9 +34,7 @@ const Collage = () => {
         <div>
           <p>{sorted?.date}</p>
           <h3> {sorted?.name} </h3>
-          <p>From array index {id}</p>
         </div>
-        <div></div>
       </div>
       <GalleryCollage items={sortedArray} />
     </section>
