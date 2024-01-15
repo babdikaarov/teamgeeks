49a69174985a2295a8b1b72bd1e7696fbe5e0926
Beksultan A <babdikaarov@gmail.com>
2024-01-16 02:11:43 +0300

    header/footer position  width > 1440px

--- src/UI/Footer/FooterTemplate.tsx
+++ src/UI/Footer/FooterTemplate.tsx
@@ -2,76 +2,77 @@ import { FC } from "react";
 import styles from "./_footer.module.scss";
 import FooterBox from "./FooterBox";
 import Logo from "../Logo/Logo";
-import SocialLinks from "../socialLinks/SocialLinks";
+// import SocialLinks from "../socialLinks/SocialLinks";
 import staticData from "./staticData.json";
 import formatPhoneNumberToText from "../../modules/formatPhoneNumberToText";
 import createMailtoLink from "../../modules/createMailtoLink";
 import createWhatsAppLink from "../../modules/createWhatsAppLink";
 import { FooterTemplateProps } from "./types";
-import { tempLinks } from "../../tempData/getHeaderData";
+// import { tempLinks } from "../../tempData/getHeaderData";
 import { Target } from "./types";
 const FooterTemplate: FC<FooterTemplateProps> = ({ bandPage, backendData }) => {
-   const notFoundData = [{ text: "Data not found", link: "" }];
+  const notFoundData = [{ text: "Data not found", link: "" }];
 
-   return (
-      <footer className={styles.footer}>
-         <div className={styles.footerLogos}>
-            <Logo bandPage={bandPage} />
-            <Logo bandPage={!bandPage} footerHide={styles.footerHide} />
-         </div>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerLogos}>
+        <Logo bandPage={bandPage} />
+        <Logo bandPage={!bandPage} footerHide={styles.footerHide} />
+      </div>
 
-         <div className={styles.footerInfo}>
-            <FooterBox
-               className={styles.footerBoxEdu}
-               header={staticData.education.header}
-               target={Target.noBlank}
-               items={staticData.education.items.map((item) => {
-                  return { text: item, link: "/studio/#teachers" };
-               })}
-            />
-            <FooterBox
-               className={styles.footerBoxAddress}
-               header={staticData.address.header}
-               target={Target.blank}
-               items={
-                  backendData?.address.map((el) => {
-                     return { text: (<span>{el.text}</span>) as unknown as string, link: el.link };
-                  }) ?? notFoundData
-               }
-            />
-            <FooterBox
-               className={styles.footerBoxContacts}
-               header={staticData.contacts.header}
-               target={Target.blank}
-               items={
-                  backendData?.contacts.map((item, i) => {
-                     let formatText: string = "";
-                     let formatLink: string = "";
+      <div className={styles.footerInfo}>
+        <FooterBox
+          className={styles.footerBoxEdu}
+          header={staticData.education.header}
+          target={Target.noBlank}
+          items={staticData.education.items.map((item) => {
+            return { text: item, link: "/studio/#teachers" };
+          })}
+        />
+        <FooterBox
+          className={styles.footerBoxAddress}
+          header={staticData.address.header}
+          target={Target.blank}
+          items={
+            backendData?.address.map((el) => {
+              return { text: (<span>{el.text}</span>) as unknown as string, link: el.link };
+            }) ?? notFoundData
+          }
+        />
+        <FooterBox
+          className={styles.footerBoxContacts}
+          header={staticData.contacts.header}
+          target={Target.blank}
+          items={
+            backendData?.contacts.map((item, i) => {
+              let formatText: string = "";
+              let formatLink: string = "";
 
-                     if (typeof item.link === "number") {
-                        formatLink = createWhatsAppLink(item.link, item.text);
-                        formatText = (
-                           <>
-                              <span>{formatPhoneNumberToText(item.link)}</span> {staticData.contacts.items[i]}
-                           </>
-                        ) as unknown as string;
-                     } else if (typeof item.link === "string") {
-                        formatLink = createMailtoLink(item.link, item.text);
-                        formatText = (<span>{item.link}</span>) as unknown as string;
-                     }
-                     return {
-                        text: formatText,
-                        link: formatLink,
-                     };
-                  }) ?? notFoundData
-               }
-            />
-         </div>
+              if (typeof item.link === "number") {
+                formatLink = createWhatsAppLink(item.link, item.text);
+                formatText = (
+                  <>
+                    <span>{formatPhoneNumberToText(item.link)}</span> {staticData.contacts.items[i]}
+                  </>
+                ) as unknown as string;
+              } else if (typeof item.link === "string") {
+                formatLink = createMailtoLink(item.link, item.text);
+                formatText = (<span>{item.link}</span>) as unknown as string;
+              }
+              return {
+                text: formatText,
+                link: formatLink,
+              };
+            }) ?? notFoundData
+          }
+        />
+      </div>
+      {/* FIXME design unclear upon width >  1440  
          <div className={styles.footerSocialPositoin}>
             <SocialLinks links={tempLinks} />
-         </div>
-      </footer>
-   );
+         </div> */}
+    </footer>
+  );
 };
 
 export default FooterTemplate;
