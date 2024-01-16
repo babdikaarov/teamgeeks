7398ea5e931b7519f990ceaf43a39d2c69919f52
Beksultan A <babdikaarov@gmail.com>
2024-01-16 04:48:52 +0300

    top buttons design change

--- src/UI/Header/topButtons/TopButtons.tsx
+++ src/UI/Header/topButtons/TopButtons.tsx
@@ -1,40 +1,41 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import styles from "../../buttons/buttons/_topButtons.module.scss";
-import mainElementAnimation from "../../../modules/mainElementAnimation";
+import styles from "./_topButtons.module.scss";
+// import mainElementAnimation from "../../../modules/mainElementAnimation";
+import bodyElementAnimation from "../../../modules/bodyElementAnimation";
 
 const TopButtons = () => {
-   const { pathname } = useLocation();
-   const [activeButton, setActiveButton] = useState<boolean>();
-   useEffect(() => {
-      const studioPathRegex = /^\/studio($|\/.*|#.*)/;
+  const { pathname } = useLocation();
+  const [activeButton, setActiveButton] = useState<boolean>();
+  useEffect(() => {
+    const studioPathRegex = /^\/studio($|\/.*|#.*)/;
 
-      if (studioPathRegex.test(pathname)) {
-         setActiveButton(true);
-      } else {
-         setActiveButton(false);
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [pathname]);
+    if (studioPathRegex.test(pathname)) {
+      setActiveButton(true);
+    } else {
+      setActiveButton(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
 
-   return (
-      <div className={styles.navButtons}>
-         <Link
-            to=""
-            className={!activeButton ? styles.navButtonsMainActive : styles.navButtonsMainNotActive}
-            onClick={() => activeButton && mainElementAnimation()}
-         >
-            <p>Cool Band</p>
-         </Link>
-         <Link
-            to="/studio"
-            className={activeButton ? styles.navButtonsSecondaryActive : styles.navButtonsSecondaryNotActive}
-            onClick={() => !activeButton && mainElementAnimation()}
-         >
-            <p>Cool Studio</p>
-         </Link>
-      </div>
-   );
+  return (
+    <div className={styles.navButtons}>
+      <Link
+        to=""
+        className={!activeButton ? styles.navButtonsMainActive : styles.navButtonsMainNotActive}
+        onClick={() => activeButton && bodyElementAnimation()}
+      >
+        <p>Cool Band</p>
+      </Link>
+      <Link
+        to="/studio"
+        className={activeButton ? styles.navButtonsSecondaryActive : styles.navButtonsSecondaryNotActive}
+        onClick={() => !activeButton && bodyElementAnimation()}
+      >
+        <p>Cool Studio</p>
+      </Link>
+    </div>
+  );
 };
 
 export default TopButtons;
