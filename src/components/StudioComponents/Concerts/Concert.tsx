c9d3d4467fcd6e4dadce6aecff27a261bab21db7
Beksultan Abdikaarov <babdikaarov@gmail.com>
2024-01-16 10:34:45 +0000

    Revert "Merge branch 'Cool-24' into 'dev'"
    This reverts merge request !47

--- src/components/StudioComponents/Concerts/Concert.tsx
+++ src/components/StudioComponents/Concerts/Concert.tsx
@@ -1,4 +1,13 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import ConcertImg1 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg2 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg3 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg4 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg5 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg6 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg7 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg8 from "../../../assets/coolstudio/Concert.jpeg";
+import ConcertImg9 from "../../../assets/coolstudio/Concert.jpeg";
 import styles from "./_concerts.module.scss";
 import icon from "../Concerts/iconSwipe";
 import { Link } from "react-router-dom";
@@ -6,81 +15,60 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import SectionWrapper from "../../../UI/SectionWrapper/SectionWrapper";
-import { useState, useEffect } from "react";
-import tempData from "./tempData";
-interface CardProps {
-   img: string;
-   header: string;
-   location: string;
-   month: string;
-}
-[];
-
+type CardProps = {
+   card: string;
+};
 
 const ConcertCard = (props: CardProps) => {
    return (
       <div className={styles.ConcertsCard}>
          <div className={styles.front}>
-            <img className={styles.concertImg} src={props.img} alt="" />
-            <p className={styles.Summer}>{props.header}</p>
-            <p className={styles.Bishkek}>{props.location}</p>
-            <p className={styles.december}>{props.month}</p>
+            <img className={styles.concertImg} src={props.card} alt="" />
+            <p className={styles.Summer}>Summer Fest</p>
+            <p className={styles.Bishkek}>Бишкек</p>
+            <p className={styles.december}>Декабрь</p>
          </div>
       </div>
    );
 };
-const [width, setWidth] = useState<number>(window.innerWidth);
-const concertImages = tempData;
-
-const mobileArray = concertImages.slice(0, 3);
-
-const mobileWidth = 700;
-useEffect(() => {
-   const handleResize = () => {
-      setWidth(window.innerWidth);
-   };
-
-   window.addEventListener("resize", handleResize);
-   window.addEventListener("resize", () => console.log(window.innerWidth));
-
-   return () => {
-      window.removeEventListener("resize", handleResize);
-   };
-}, []);
-
 
 const Concert = () => {
+   const concertImages = [
+      ConcertImg1,
+      ConcertImg2,
+      ConcertImg3,
+      ConcertImg4,
+      ConcertImg5,
+      ConcertImg6,
+      ConcertImg7,
+      ConcertImg8,
+      ConcertImg9,
+   ];
 
    return (
       <SectionWrapper header="Отчетные концерты">
          <div>
             <div className={styles.ConcertSlider}>
-               {width < mobileWidth ? (
-                  mobileArray.map((card, i) => <ConcertCard key={i} {...card} />)
-               ) : (
-                  <>
-                     <Swiper
-                        spaceBetween={0}
-                        slidesPerView={3}
-                        navigation={{ nextEl: "#btn1", prevEl: "#btn2" }}
-                        modules={[Navigation]}
-                     >
-                        {concertImages.map((image, index) => (
-                           <SwiperSlide key={index}>
-                              <Link to={"/gallery/10"}>
-                                 <ConcertCard {...image} />
-                              </Link>
-                           </SwiperSlide>
-                        ))}
-                     </Swiper>
-                     <div id="btn2" className={styles.LeftSwip}>
-                        {icon}
-                     </div>
-                     <div id="btn1" className={styles.RightSwip}>
-                        {icon}
-                     </div>
-                  </>
-               )}
+               <Swiper
+                  spaceBetween={0}
+                  slidesPerView={3}
+                  navigation={{ nextEl: "#btn1", prevEl: "#btn2" }}
+                  modules={[Navigation]}
+               >
+                  {concertImages.map((image, index) => (
+                     <SwiperSlide key={index}>
+                        <Link to={"/gallery/10"}>
+                           <ConcertCard card={image} />
+                        </Link>
+                     </SwiperSlide>
+                  ))}
+               </Swiper>
+               <div id="btn2" className={styles.LeftSwip}>
+                  {icon}
+               </div>
+               <div id="btn1" className={styles.RightSwip}>
+                  {icon}
+               </div>
                <a className={styles.watchmore} href="/gallery">
                   Смотреть еще
                </a>
