4ca9b8591020aaa29f66a9393856127e1a64d0bd
Ganyev Aman <ganyevaman7@gmail.com>
2024-01-22 19:27:47 +0600

    OurTeam new images added

--- src/tempData/getTeamList.ts
+++ src/tempData/getTeamList.ts
@@ -1,69 +1,78 @@
 const teamList = [
    {
       id: 1,
-      img: "/ourTeam/anara-vocal.jpg",
+      img: "/ourTeam/Anarkin.jpg",
+      name: "Anarkin",
       role: "Вокал",
-      name: "Анара",
       view: "portrait",
+      video: "/ourTeam/Anarkin.mp4"
    },
    {
       id: 2,
-      img: "/ourTeam/davlet-baraban.JPG",
-      role: "Барабаны",
-      name: "Давлет",
+      img: "/ourTeam/Erbol.jpg",
+      name: "Эрбол",
+      role: "Гитара",
       view: "portrait",
+      video: "/ourTeam/Erbol.mp4"
    },
    {
       id: 3,
-      img: "/ourTeam/djKhan.JPG",
-      role: "DJ",
-      name: "DJ Khan",
+      img: "/ourTeam/Suliko.jpg",
+      name: "Сулико",
+      role: "Бас-Гитара/Вокал",
       view: "portrait",
+      video: "/ourTeam/Suliko.mp4"
    },
    {
       id: 4,
-      img: "/ourTeam/djOlymp.jpg",
-      role: "DJ",
-      name: "DJ OLYMP",
+      img: "/ourTeam/Davlet.jpg",
+      name: "Давлет",
+      role: "Барабаны",
       view: "portrait",
+      video: "/ourTeam/Davlet.mp4"
    },
    {
       id: 5,
-      img: "/ourTeam/erbol-gitara.jpg",
-      role: "Гитара",
-      name: "Ербол",
+      img: "/ourTeam/Osmany.jpg",
+      name: "Османи",
+      role: "Перкуссия/Вокал",
       view: "portrait",
+      video: "/ourTeam/Osmany.mp4"
    },
    {
       id: 6,
-      img: "/ourTeam/keno-voice-producer.jpg",
-      role: "Звукорежисер",
-      name: "Keno",
+      img: "/ourTeam/Dj Khan.jpg",
+      name: "DJ Khan",
+      role: "",
       view: "portrait",
+      video: "/ourTeam/Dj Khan.mp4"
    },
    {
       id: 7,
-      img: "/ourTeam/osman-vocal.jpg",
-      role: "Перкуссия Вокал",
-      name: "Осман",
+      img: "/ourTeam/Dj Olymp.jpg",
+      name: "DJ Olymp",
+      role: "",
       view: "portrait",
+      video: "/ourTeam/Dj Olymp.mp4"
    },
    {
       id: 8,
-      img: "/ourTeam/sultan-bass.jpeg",
-      role: "Басс-Гитара Вокал",
-      name: "Султан",
+      img: "/ourTeam/Keno.jpg",
+      name: "Keno",
+      role: "Звукорежиссер",
       view: "portrait",
+      video: "/ourTeam/Keno.mp4"
    },
    {
       id: 9,
       img: "/ourTeam/danceshow.jpg",
-      role: "",
       name: "Dance Show",
+      role: "",
       view: "landscape",
+      video: "/ourTeam/danceshow.mp4"
    },
 ];
 
-const addAvailableVideo = teamList.map((el) => Object.assign(el, { video: "/ourTeam/flipVideo.mp4" }));
+const addAvailableVideo = teamList.map((el) => Object.assign(el));
 
 export default addAvailableVideo;
