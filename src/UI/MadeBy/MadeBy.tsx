5eb29f0ad2a8f695eaa222f82a3a45e14f2c19c1
Shayirbek Abdymomunov <legionera803@gmail.com>
2024-02-05 21:55:05 +0600

    cool-143 is refactored

--- src/UI/MadeBy/MadeBy.tsx
+++ src/UI/MadeBy/MadeBy.tsx
@@ -2,7 +2,7 @@ import styles from "./_madeBy.module.scss";
 const MadeBy = () => {
    return (
       <>
-         <a href="#" className={styles.made}>Made by Geeks Pro</a>
+         <a href="https://www.instagram.com/geeks_pro/" className={styles.made}>Made by Geeks Pro</a>
       </>
    );
 };
