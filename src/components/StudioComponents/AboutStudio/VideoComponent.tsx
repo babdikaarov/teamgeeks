3f4b37b4d2f7bc222271b82c2a6f22706bd6c8bb
Beksultan A <babdikaarov@gmail.com>
2024-02-13 02:06:15 +0300

    refactor: COOL-183 rebase against dev branch

--- src/components/StudioComponents/AboutStudio/VideoComponent.tsx
+++ src/components/StudioComponents/AboutStudio/VideoComponent.tsx
@@ -1,21 +1,17 @@
 // modules
-import { useEffect, useRef, useState  } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+// import { extractYouTubeVideoId } from "../../../modules/extractAllYouTubeVideoID";
 import { getCoolStudio } from "../../../store/aboutStudioThunk";
-// import { extractYouTubeVideoId } from "../../../../modules/extractAllYouTubeVideoID";
 // styles
 import styles from "./_videoComponent.module.scss";
-
-
-
-
-
 
 const VideoComponent = () => {
    const dispatch = useAppDispatch();
    const coolStudioData = useAppSelector((state) => state.getCoolStudioVideo.data);
    const videoRef = useRef<HTMLIFrameElement | null>(null);
-      const [userPlayedVideo, setUserPlayedVideo] = useState(false);
+   const [userPlayedVideo] = useState(false);
+
    useEffect(() => {
       dispatch(getCoolStudio());
    }, [dispatch]);
@@ -23,15 +19,10 @@ const VideoComponent = () => {
    useEffect(() => {
       const handleVisibilityChange = (entries: IntersectionObserverEntry[]) => {
          const [entry] = entries;
-
-         // Check if the video is in the viewport
          if (entry.isIntersecting) {
-            // Only play video if the user has explicitly played it
-            if (userPlayedVideo) {
+            userPlayedVideo &&
                videoRef.current?.contentWindow?.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
-            }
          } else {
-            // Pause video when out of viewport
             videoRef.current?.contentWindow?.postMessage('{"event":"command","func":"pauseVideo","args":""}', "*");
          }
       };
@@ -49,24 +40,19 @@ const VideoComponent = () => {
       };
    }, [userPlayedVideo]);
 
-
-   const handleUserPlay = () => {
-      setUserPlayedVideo(true);
-      videoRef.current?.contentWindow?.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
-   };
-
    if (!coolStudioData) {
       return <div>Loading...</div>;
    }
+
    // const videoId = extractYouTubeVideoId(coolStudioData.youtubeUrl);
+
    return (
       <div>
-          <div onClick={handleUserPlay}></div>
          <iframe
-         ref={videoRef}
+            ref={videoRef}
             className={styles.video}
-            src={`https://www.youtube.com/embed/uy-nFAuzFf4`} //FIX_ME remove and uncomment below after backend
-            // src={`https://www.youtube.com/embed/${videoId}`}
+            src={`https://www.youtube.com/embed/uy-nFAuzFf4?enablejsapi=1`} //FIX_ME remove and uncomment below after backend
+            // src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1`}
             title="YouTube video player"
             allow="autoplay; encrypted-media"
             sandbox="allow-scripts allow-same-origin"
