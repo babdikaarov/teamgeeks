138c370388162a29988342cc12321a3d33e35443
Beksultan A <babdikaarov@gmail.com>
2024-02-22 01:25:52 +0300

    fix: COOL-215 feedback initial slide bug

--- src/components/StudioComponents/StudentsFeedback/StudentsFeedback.tsx
+++ src/components/StudioComponents/StudentsFeedback/StudentsFeedback.tsx
@@ -16,91 +16,96 @@ import styles from "./_studentsFeedback.module.scss";
 const StudentsFeedback = () => {
    const [initialSlide, setInitialSlide] = useState<number>(1);
    const dispatch = useAppDispatch();
-   const data = useAppSelector((state) => state.getStudentReviwes.data)!;
+   const { data, getLoading } = useAppSelector((state) => state.getStudentReviwes)!;
    useEffect(() => {
       dispatch(getStudentReviwes());
-      setInitialSlide(Math.floor(data.length / 2));
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [dispatch]);
-   // console.log(data)
+   useEffect(() => {
+      if (!data) return;
+      if (data.length > 0) {
+         setInitialSlide(Math.floor(data.length / 2));
+      }
+   }, [data]);
 
    return (
       <SectionWrapper header={"Отзывы наших студентов"}>
          <div className={styles.feedbackContainer}>
             <NavigationButton id="FeedbackPrev" />
-            <Swiper
-               className={styles.feedbackSwiper}
-               effect={"coverflow"}
-               grabCursor
-               centeredSlides
-               initialSlide={initialSlide}
-               // initialSlide={2}
-               loop
-               coverflowEffect={{
-                  rotate: 0,
-                  stretch: 0,
-                  depth: 100,
-                  modifier: 2.5,
-               }}
-               freeMode
-               mousewheel={{
-                  forceToAxis: true,
-               }}
-               navigation={{ nextEl: "#FeedbackNext", prevEl: "#FeedbackPrev" }}
-               modules={[Mousewheel, Navigation, FreeMode, EffectCoverflow]}
-               breakpoints={{
-                  1440: {
-                     slidesPerView: 3.5,
-                     spaceBetween: -66,
-                  },
-                  1200: {
-                     slidesPerView: 3.3,
-                     spaceBetween: -35,
-                  },
-                  1024: {
-                     slidesPerView: 3.35,
-                     spaceBetween: -75,
-                  },
-                  892: {
-                     slidesPerView: 3.3,
-                     spaceBetween: -85,
-                  },
-                  768: {
-                     slidesPerView: 2.8,
-                     spaceBetween: -55,
-                  },
-                  658: {
-                     slidesPerView: 2.3,
-                     spaceBetween: -30,
-                  },
-                  576: {
-                     slidesPerView: 2.3,
-                     spaceBetween: -90,
-                  },
-                  482: {
-                     slidesPerView: 1.5,
-                     spaceBetween: -50,
-                  },
-                  390: {
-                     slidesPerView: 1.5,
-                     spaceBetween: -50,
-                  },
-                  1: {
-                     slidesPerView: 1.3,
-                     spaceBetween: -40,
-                  },
-               }}
-            >
-               {data.map((card, i) => (
-                  <SwiperSlide key={i}>
-                     <StudentsFeedbackCard
-                        img={card.image}
-                        name={card.name}
-                        word={card.reviews}
-                     />
-                  </SwiperSlide>
-               ))}
-            </Swiper>
+            {!getLoading && (
+               <Swiper
+                  key={initialSlide}
+                  className={styles.feedbackSwiper}
+                  effect={"coverflow"}
+                  grabCursor
+                  centeredSlides
+                  loop
+                  coverflowEffect={{
+                     rotate: 0,
+                     stretch: 0,
+                     depth: 100,
+                     modifier: 2.5,
+                  }}
+                  freeMode
+                  mousewheel={{
+                     forceToAxis: true,
+                  }}
+                  navigation={{ nextEl: "#FeedbackNext", prevEl: "#FeedbackPrev" }}
+                  modules={[Mousewheel, Navigation, FreeMode, EffectCoverflow]}
+                  breakpoints={{
+                     1440: {
+                        slidesPerView: 3.5,
+                        spaceBetween: -66,
+                     },
+                     1200: {
+                        slidesPerView: 3.3,
+                        spaceBetween: -35,
+                     },
+                     1024: {
+                        slidesPerView: 3.35,
+                        spaceBetween: -75,
+                     },
+                     892: {
+                        slidesPerView: 3.3,
+                        spaceBetween: -85,
+                     },
+                     768: {
+                        slidesPerView: 2.8,
+                        spaceBetween: -55,
+                     },
+                     658: {
+                        slidesPerView: 2.3,
+                        spaceBetween: -30,
+                     },
+                     576: {
+                        slidesPerView: 2.3,
+                        spaceBetween: -90,
+                     },
+                     482: {
+                        slidesPerView: 1.5,
+                        spaceBetween: -50,
+                     },
+                     390: {
+                        slidesPerView: 1.5,
+                        spaceBetween: -50,
+                     },
+                     1: {
+                        slidesPerView: 1.3,
+                        spaceBetween: -40,
+                     },
+                  }}
+               >
+                  {data?.map((card, i) => (
+                     <SwiperSlide key={i}>
+                        <StudentsFeedbackCard
+                           img={card.image}
+                           name={card.name}
+                           word={card.reviews}
+                        />
+                     </SwiperSlide>
+                  ))}
+               </Swiper>
+            )}
             <NavigationButton id="FeedbackNext" />
          </div>
       </SectionWrapper>
