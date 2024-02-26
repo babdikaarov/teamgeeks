aa5a367cdf74032bb29599614cdece0f4d104c0a
Beksultan A <babdikaarov@gmail.com>
2024-02-26 03:03:15 +0300

    refactor: imageloader optim

--- src/UI/ImageLoader/ImageLoader.tsx
+++ src/UI/ImageLoader/ImageLoader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import { Blurhash } from "react-blurhash";
 import { isBlurhashValid } from "blurhash";
 import styles from "./_imageLoader.module.scss";
@@ -9,47 +9,27 @@ interface ImageLoaderProps {
 }
 
 const ImageLoader: React.FC<ImageLoaderProps> = ({ src, bluer }) => {
-   const defaultBluer = "L03us9j[fQj[offQfQfQfQfQfQfQ";
+   // const defaultBluer = "L03us9j[fQj[offQfQfQfQfQfQfQ";
    const [loaded, setLoaded] = useState(false);
-   // const [imageSrc, setImageSrc ] = useState(src)
-
-   useEffect(() => {
-      // setImageSrc(src)
-      if (src && bluer) {
-         const img = new Image();
-         img.onload = () => {
-            setTimeout(() => {
-               setLoaded(true);
-            }, 500);
-         };
-         img.src = src;
-         img.loading = "lazy";
-      }
-
-      return () => {
-         setLoaded(false);
-      };
-   }, [src, bluer]);
+   if(!isBlurhashValid(bluer).result) return
 
    return (
-      <>
-         <Blurhash
-            hash={isBlurhashValid(bluer).result ? bluer : defaultBluer}
-            className={styles.div}
-            resolutionX={32}
-            resolutionY={32}
-            punch={1}
-         />
          <Suspense>
+            <Blurhash
+               hash={bluer}
+               className={styles.div}
+               resolutionX={32}
+               resolutionY={32}
+               punch={1}
+            />
             <img
                className={`${styles.image} ${loaded ? styles.loaded : ""}`}
-               // src={imageSrc}
+               onLoad={() => setLoaded(true)}
                loading="lazy"
                src={src}
                alt="Poster About"
             />
          </Suspense>
-      </>
    );
 };
 
