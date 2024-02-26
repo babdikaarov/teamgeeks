77bd63bef0e05cba9731497ac1f856e9ab5c1d2e
Beksultan A <babdikaarov@gmail.com>
2024-02-26 03:23:41 +0300

    refactor: COOL-00 add usecallback responsivesorting

--- src/modules/hooks/useResponsiveSorting.ts
+++ src/modules/hooks/useResponsiveSorting.ts
@@ -1,54 +1,54 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const useResponsiveSorting = (arr: Slice.IGetBandImages[]): Slice.IGetBandImages[] => {
    const [sortedArray, setSortedArray] = useState<Slice.IGetBandImages[] | []>(arr);
 
-   useEffect(() => {
-      const handleResize = () => {
-         let sequence: string[];
-         const newSortedArray: Slice.IGetBandImages[] = [];
-         const width = window.innerWidth;
+   const handleResize = useCallback(() => {
+      let sequence: string[];
+      const newSortedArray: Slice.IGetBandImages[] = [];
+      const width = window.innerWidth;
 
-         if (width < 850) {
-            const portraitObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "PORTRAIT");
-            const landscapeObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "LANDSCAPE");
-            while (portraitObjects.length || landscapeObjects.length) {
-               landscapeObjects.length && newSortedArray.push(landscapeObjects.shift()!);
-               portraitObjects.length && newSortedArray.push(portraitObjects.shift()!);
-               portraitObjects.length && newSortedArray.push(portraitObjects.shift()!);
-               landscapeObjects.length && newSortedArray.push(landscapeObjects.shift()!);
-            }
-
-            setSortedArray(newSortedArray);
-         } else {
-            sequence = [
-               "PORTRAIT",
-               "LANDSCAPE",
-               "LANDSCAPE",
-               "LANDSCAPE",
-               "PORTRAIT",
-               "PORTRAIT",
-               "PORTRAIT",
-               "LANDSCAPE",
-            ];
-
-            const portraitObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "PORTRAIT");
-            const landscapeObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "LANDSCAPE");
-            let currentIndex = 0;
-            while (portraitObjects.length || landscapeObjects.length) {
-               if (sequence[currentIndex] === "PORTRAIT" && portraitObjects.length) {
-                  newSortedArray.push(portraitObjects.shift()!);
-               } else if (sequence[currentIndex] === "LANDSCAPE" && landscapeObjects.length) {
-                  newSortedArray.push(landscapeObjects.shift()!);
-               }
-
-               currentIndex = (currentIndex + 1) % sequence.length;
-            }
-
-            setSortedArray(newSortedArray);
+      if (width < 850) {
+         const portraitObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "PORTRAIT");
+         const landscapeObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "LANDSCAPE");
+         while (portraitObjects.length || landscapeObjects.length) {
+            landscapeObjects.length && newSortedArray.push(landscapeObjects.shift()!);
+            portraitObjects.length && newSortedArray.push(portraitObjects.shift()!);
+            portraitObjects.length && newSortedArray.push(portraitObjects.shift()!);
+            landscapeObjects.length && newSortedArray.push(landscapeObjects.shift()!);
          }
-      };
 
+         setSortedArray(newSortedArray);
+      } else {
+         sequence = [
+            "PORTRAIT",
+            "LANDSCAPE",
+            "LANDSCAPE",
+            "LANDSCAPE",
+            "PORTRAIT",
+            "PORTRAIT",
+            "PORTRAIT",
+            "LANDSCAPE",
+         ];
+
+         const portraitObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "PORTRAIT");
+         const landscapeObjects = arr.filter((obj: { orientation: string }) => obj.orientation === "LANDSCAPE");
+         let currentIndex = 0;
+         while (portraitObjects.length || landscapeObjects.length) {
+            if (sequence[currentIndex] === "PORTRAIT" && portraitObjects.length) {
+               newSortedArray.push(portraitObjects.shift()!);
+            } else if (sequence[currentIndex] === "LANDSCAPE" && landscapeObjects.length) {
+               newSortedArray.push(landscapeObjects.shift()!);
+            }
+
+            currentIndex = (currentIndex + 1) % sequence.length;
+         }
+
+         setSortedArray(newSortedArray);
+      }
+   }, [arr]);
+
+   useEffect(() => {
       handleResize();
 
       window.addEventListener("resize", handleResize);
@@ -57,7 +57,7 @@ const useResponsiveSorting = (arr: Slice.IGetBandImages[]): Slice.IGetBandImages
          window.removeEventListener("resize", handleResize);
          setSortedArray([]);
       };
-   }, [arr]);
+   }, [handleResize]);
 
    return sortedArray;
 };
